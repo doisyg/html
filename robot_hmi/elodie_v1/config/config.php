5d08de922b65e4f00cc7580d3c35b76223495e03
Unknown <stephane.morillon@smorillon.com>
2019-12-22 09:40:24 +0100

    Init robot HMI

---
+++ robot_hmi/elodie_v1/config/config.php
@@ -0,0 +1,5 @@
+<?php
+$_CONFIG['URL'] = 'https://elodie.wyca-solutions.com/robot_hmi/elodie_v1/';
+$_CONFIG['URL_API'] = 'https://elodie.wyca-solutions.com/API/';
+$_CONFIG['URL_ROBOT'] = 'https://elodie.wyca-solutions.com/robot_phy/elodie_v1/';
+?>
