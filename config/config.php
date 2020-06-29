30919e92614a1c1e449487fd36d7874b7f90b60b
Unknown <stephane.morillon@smorillon.com>
2020-06-29 11:55:40 +0200

    Remove old files

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 $_CONFIG = array();
 $_CONFIG['URL'] = "https://elodie.wyca-solutions.com/";
 $_CONFIG['URL_API'] = $_CONFIG['URL']."API/";
-$_CONFIG['URL_SERVER_TODO'] = "https://traxdev.wyca-solutions.com:4321";
+$_CONFIG['URL_SERVER_TODO'] = "https://integrator.wyca-solutions.com:7458";
 
 $_CONFIG['DIR_SECURE'] = dirname(__FILE__).'/../_secure_/';
 
@@ -10,10 +10,6 @@ $_CONFIG['BD_HOST'] = "127.0.0.1";
 $_CONFIG['BD_USER'] = "wyca_elodie_app";
 $_CONFIG['BD_PASSWORD'] = "www48ElodieAPP";
 $_CONFIG['BD_BD'] = "wyca_elodie_app";
-
-//$_CONFIG['CONFIG_PATH'] = "/var/www/html/robot_config/";
-$_CONFIG['CONFIG_PATH'] = dirname(__FILE__).'/../robot_config/';
-
 
 $_CONFIG['KEY'] = '&xTYhx+r.C|G/cK&o,9s-JXE|:EZ)IWF6481FKMN&*E(OAYK-%LOR-eM#2=1-<oN';
 $_CONFIG['WS_KEY'] = 'Q5rd9`#4<VqP0|b4^f%jSUk(1L`DCqEE40lASD9px+r~Wqp.DZ8eiRz*UP2gXF0*';
