2359513dc61a9c9b733bda6b55fa096fcc13e82b
Unknown <stephane.morillon@smorillon.com>
2020-07-09 10:45:51 +0200

    Force refresh js

--- template/footer.php
+++ template/footer.php
@@ -40,7 +40,7 @@
         <script src="<?php echo $_CONFIG['URL'];?>assets/vendor/svg-pan-zoom/svg-pan-zoom.js"></script>
 		<script src="<?php echo $_CONFIG['URL'];?>assets/vendor/svg-pan-zoom/hammer.js"></script>
 
-		<?php $lastUpdate = '20200709';?>
+		<?php $lastUpdate = '20200709_2';?>
 
 		<script src="<?php echo $_CONFIG['URL'];?>assets/javascripts/plugins.js?v=<?php echo $lastUpdate;?>"></script>
 		<script src="<?php echo $_CONFIG['URL'];?>assets/javascripts/joystick.js?v=<?php echo $lastUpdate;?>"></script>
