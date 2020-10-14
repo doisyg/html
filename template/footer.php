e70818d14c38d9540bade1325a418f2ba87a96cc
Guillaume <guillaume.doisy@wyca.fr>
2020-10-14 11:31:16 +0200

    Update URL local steph to generique

--- template/footer.php
+++ template/footer.php
@@ -47,7 +47,7 @@
         <script src="<?php echo $_CONFIG['URL'];?>assets/vendor/svg-pan-zoom/svg-pan-zoom.js"></script>
 		<script src="<?php echo $_CONFIG['URL'];?>assets/vendor/svg-pan-zoom/hammer.js"></script>
 
-		<?php $lastUpdate = '20201006';?>
+		<?php $lastUpdate = '20201014';?>
 		<script src="<?php echo $_CONFIG['URL'];?>assets/javascripts/plugins.js?v=<?php echo $lastUpdate;?>"></script>
 		<script src="<?php echo $_CONFIG['URL'];?>assets/javascripts/joystick.js?v=<?php echo $lastUpdate;?>"></script>
 		<script src="<?php echo $_CONFIG['URL'];?>assets/javascripts/robot.js?v=<?php echo $lastUpdate;?>"></script>
