03e5bf2432cc9445d1929ec27c8f5c49c7b5e23b
YvanC <yvan.combarieu@wyca.fr>
2021-01-14 13:42:49 +0100

    Add url config for new PC Yvan

--- template/footer.php
+++ template/footer.php
@@ -39,8 +39,8 @@
         <script>
 		var lang = '<?php echo $currentLang->iso;?>';
 
-		var robot_host = '<?php echo (file_exists('C:\\'))?((file_exists('C:\\Users\\F'))?'10.0.0.39:'.($server_request_scheme == 'http'?'9094':'9095'):'192.168.0.33:'.($server_request_scheme == 'http'?'9094':'9095')):'wyca.run:'.($server_request_scheme == 'http'?'9094':'9095');?>';
-		var robot_http = '<?php echo (file_exists('C:\\'))?((file_exists('C:\\Users\\F'))?$server_request_scheme.'://10.0.0.39':$server_request_scheme.'://192.168.0.33'):'';?>';
+		var robot_host = '<?php echo (file_exists('C:\\'))?((file_exists('C:\\Users\\F') || file_exists('C:\\Users\\Yvan')?'10.0.0.39:'.($server_request_scheme == 'http'?'9094':'9095'):'192.168.0.33:'.($server_request_scheme == 'http'?'9094':'9095')):'wyca.run:'.($server_request_scheme == 'http'?'9094':'9095');?>';
+		var robot_http = '<?php echo (file_exists('C:\\'))?((file_exists('C:\\Users\\F') || file_exists('C:\\Users\\Yvan')?$server_request_scheme.'://10.0.0.39':$server_request_scheme.'://192.168.0.33'):'';?>';
 		//var robot_host = '<?php echo (file_exists('C:\\'))?'10.0.0.44:'.($server_request_scheme == 'http'?'9094':'9095'):'wyca.run:'.($server_request_scheme == 'http'?'9094':'9095');?>';
 		var use_ssl = <?php echo $server_request_scheme == 'http'?'false':'true';?>;
 		var app_url = '<?php echo $server_request_scheme;?>://wyca.run';
