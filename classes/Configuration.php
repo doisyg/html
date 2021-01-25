1e1984d176edb23df18dc3f31833411b4043860f
Elodie-dev <guillaume.doisy@wyca.fr>
2021-01-25 15:14:40 +0100

    Add new c.conf

--- classes/Configuration.php
+++ classes/Configuration.php
@@ -5,11 +5,14 @@ class Configuration
 	
 	public static function Init()
 	{
+		global $_CONFIG;
+
 		//echo exec('whoami');
 		if(isset($_CONFIG['CONF_PATH']))
 			$file_path = $_CONFIG['CONF_PATH'];
 		else
 			$file_path = dirname(__FILE__).'/../lang/c.conf';
+		//echo $file_path;
 		try{
 			if (file_exists($file_path))
 			{
@@ -26,13 +29,33 @@ class Configuration
 				}
 			}
 			else{
-				self::$data = array();
-				if ($fd = fopen($file_path, 'w')){
-					//CREER C.CONF IF DO NOT EXIST
-					fwrite($fd, json_encode(self::$data));
-					fclose($fd);
-				}else{
-					throw new Exception('File not found and unable to create it.');
+			
+				if(file_exists(dirname(__FILE__).'/../lang/c.conf'))
+				{
+					//SI OLD c.conf EXIST COPY + LOAD IT
+					copy( dirname(__FILE__).'/../lang/c.conf', $file_path);
+					$content =  file_get_contents ( $file_path );
+					if ( !$content ) {
+						self::$data = array();
+						throw new Exception('File open failed.');
+					}else{
+						self::$data = json_decode($content, true);
+						if (json_last_error() != JSON_ERROR_NONE){
+							self::$data = array();
+							throw new Exception('Json decode failed.');
+						}
+					}
+				}
+				else
+				{
+					self::$data = array();
+					if ($fd = fopen($file_path, 'w')){
+						//CREER C.CONF IF DO NOT EXIST
+						fwrite($fd, json_encode(self::$data));
+						fclose($fd);
+					}else{
+						throw new Exception('File not found and unable to create it.');
+					}
 				}
 			}
 		}
@@ -44,10 +67,12 @@ class Configuration
 	
 	public static function Save()
 	{
+		global $_CONFIG;
 		if(isset($_CONFIG['CONF_PATH']))
 			$file_path = $_CONFIG['CONF_PATH'];
 		else
 			$file_path = dirname(__FILE__).'/../lang/c.conf';
+
 		if ($fd = fopen($file_path, 'w'))
 		{	
 			fwrite($fd, json_encode(self::$data));
