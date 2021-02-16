97e9792341157a24a226f4759b8a38223e27cb71
Yvan COMBARIEU <yvan.combarieu@wyca.fr>
2021-02-16 14:04:45 +0100

    Add git branch to lastUpdate for js/css files

--- config/initSite.php
+++ config/initSite.php
@@ -31,6 +31,47 @@ if ( (! empty($_SERVER['REQUEST_SCHEME']) && $_SERVER['REQUEST_SCHEME'] == 'http
 $_CONFIG['URL_API'] = $_CONFIG['URL']."API/";
 
 $_CONFIG['MODE'] = file_exists('C:\\')? 'DEV':'PROD';
+
+// GET GIT BRANCH
+if(file_exists('.git/HEAD')){
+	$stringfromfile = file('.git/HEAD', FILE_USE_INCLUDE_PATH);
+
+	$firstLine = $stringfromfile[0]; //get the string from the array
+	$explodedstring = explode("/", $firstLine, 3); //seperate out by the "/" in the string
+	$branchname = $explodedstring[2]; //get the one that is always the branch name
+	$lc = substr($branchname,-1);
+	if($lc == "\n" || $lc == "\r"|| $lc == "\t");
+		$branchname = substr($branchname,0,strlen($branchname)- 1);
+
+	$bn = '';
+	switch($branchname){
+		case 'dev': $bn = 'dev'; break;
+		case 'dev_yvan': $bn = 'dyv'; break;
+		case 'master': $bn = 'mas'; break;
+		case 'html_stable': $bn = 'sta'; break;
+		case 'html_release': $bn = 'rel'; break;
+		case 'multimap': $bn = 'mum'; break;
+
+		default : 
+			$explode = explode('_',$branchname);
+			if(count($explode) > 1){
+				foreach ($explode as $key => $value){
+					$bn .= substr($value,0,1);
+				}
+			}
+			else
+				$bn = substr($branchname,0,2);
+		break;
+	}
+	$version = date('Ymd').'_'.$bn;
+	if($bn == 'rel'){
+		// DO SMTHING SPECIAL ON RELEASE ?
+		// STOCK DATE WRITE IT ON CONF FILE ?
+	}
+}else{
+	$_CONFIG['MODE'] = 'PROD';
+	$version = date('Ymd');
+}
 $_CONFIG['CONF_PATH'] = $_CONFIG['MODE'] == 'DEV' ? dirname(__FILE__).'/../lang/c.conf' : dirname(__FILE__).'/../../conf/c.conf';
 
 require_once (dirname(__FILE__)."/../lib/lib.php");
